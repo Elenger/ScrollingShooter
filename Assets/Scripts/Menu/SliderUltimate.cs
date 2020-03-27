6a6c289e7e11acdbdd8442895d834063098f7b95
Elenger <orion96king@gmail.com>
2020-03-27 19:15:47 +0300

    Add the Ultimate

---
+++ Assets/Scripts/Menu/SliderUltimate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderUltimate : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Ultimate _ultimate;
+
+    public void ChangeSliderFilling(float score)
+    {
+        score = score % 1001;
+        _slider.value = score / 1000;
+
+        if (_slider.value == 1)
+        {
+            _ultimate.UseUltimate();
+            _slider.value = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Ultimate/Ultimate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ultimate : MonoBehaviour
+{
+    [SerializeField] private GameObject _bulletCurrently;
+    [SerializeField] private GameObject _bulletUltimative;
+    public void UseUltimate()
+    {
+        FirstUltimate();
+    }    
+
+    private void FirstUltimate()
+    {
+        BulletPooler.SharedInstance.SetNewPrefab(_bulletUltimative);
+        StartCoroutine(Countdown());
+    }
+
+    private IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(4f);
+        BulletPooler.SharedInstance.SetNewPrefab(_bulletCurrently);
+    } 
+}

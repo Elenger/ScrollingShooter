c424df59ad0489861e698dbcbd4dc191721332c7
ZROS <zros98@gmail.com>
2020-03-31 01:47:58 +0200

    Add In Game Menu

---
+++ Assets/Scripts/Menu/GameTimeScale.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTimeScale : MonoBehaviour
+{
+    [SerializeField] private int _gameState;
+    public void PauseGame()
+    {
+        Time.timeScale = _gameState;
+    }
+}

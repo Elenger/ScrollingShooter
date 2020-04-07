b71cf629f63e8367dd8e1ff96ff7a173f1cbc4ea
Elenger <orion96king@gmail.com>
2020-04-07 23:12:56 +0300

    Fixed ability button

--- Assets/Scripts/Ability/Ability.cs
+++ Assets/Scripts/Ability/Ability.cs
@@ -11,18 +11,6 @@ public class Ability : MonoBehaviour
     [SerializeField] private Button _buttonAbility;
     [SerializeField] private Image _imageAbility;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void UseAbility()
     {
         _buttonAbility.interactable = false;
